cb48b12805121446c58e98392418e40221ee232d
Yue Huang <806628409@qq.com>
2025-04-03 22:13:37 +0800

    修改了跨周调整的bug

--- miniprogram/pages/view_apply_details/view_apply_details.js
+++ miniprogram/pages/view_apply_details/view_apply_details.js
@@ -10,39 +10,8 @@ Page({
     new_time: '',
     type: '',
   },
-  accept(){
-    if(this.data.loading) return
-    this.setData({
-      loading: true
-    })
 
-    if (this.data.request_detail.is_vote){
-      var team_this = app.globalData.leader_info.team
-      var voted_accept = this.data.request_detail.voted_accept
-      voted_accept.push(team_this)
-      var voted_reject = this.data.request_detail.voted_reject
-
-      wx.cloud.callFunction({
-        name: "vote_request",
-        data:{
-          request: this.data.request_detail,
-          voted_accept: voted_accept,
-          voted_reject: voted_reject
-        },
-        success: res=>{
-          console.log(res)
-          wx.navigateBack()
-
-          return
-        },
-
-        fail: err=>{
-          console.log(res)
-          return
-        }
-      })
-    }
-
+  checkRequestValidation(){
     var now = new Date()
     var time_ddl = new Date(
       now.getFullYear(),
@@ -51,57 +20,66 @@ Page({
     )
 
     if (time_ddl.getTime()<this.data.request_detail.time_new.getTime() 
-    && time_ddl.getTime()<this.data.request_detail.time.getTime()){
-      if(this.data.request_detail.type==3){
-        var new_state = 4
-      }
-      else if(this.data.request_detail.type==1){
-        var new_state = 2
-      }
-      wx.cloud.callFunction({
-        name: "update_request",
-        data:{
-          request:this.data.request_detail,
-          new_state: new_state,
-          to_delete: false
-        },
-        success: res => {
-          app.globalData.request_detail.state = 2
-
-          var text_email = "原比赛: " + app.globalData.request_detail.month+'月'+app.globalData.request_detail.date+'日 ' 
-          + app.globalData.request_detail.hour+':'+app.globalData.request_detail.minute + " "
-          + app.globalData.request_detail.home_team + " VS "+ app.globalData.request_detail.away_team
-          + ' ' + app.globalData.request_detail.place + '\n' 
-          + "调整后比赛: "+ app.globalData.request_detail.month_new+'月'+app.globalData.request_detail.date_new+'日 ' 
-          + app.globalData.request_detail.hour_new+':'+app.globalData.request_detail.minute_new + " "
-          + app.globalData.request_detail.home_team + " VS "+ app.globalData.request_detail.away_team
-          + ' ' + app.globalData.request_detail.place_new + '\n'
-          + "申请日期: "+app.globalData.request_detail.month_req+'月'+app.globalData.request_detail.date_req+'日 ' 
-          + app.globalData.request_detail.hour_req+':'+app.globalData.request_detail.minute_req + '\n'
-          + "申请方: "+app.globalData.request_detail.requester +'\n'
-          + "申请类型: "+app.globalData.TYPES[app.globalData.request_detail.type-1] + '\n'
-          + "组别: " + app.globalData.request_detail.group + '\n'
-          console.log(text_email)
-          wx.cloud.callFunction({
-            name: 'send_email',
-            data:{
-              text: text_email,
-              attachment: 0,
-              subject: app.globalData.TYPES[app.globalData.request_detail.type-1]+'申请 '+ app.globalData.request_detail.home_team + " VS "+ app.globalData.request_detail.away_team + " (协商成功)"
-            },
-            success: res =>{
-              console.log(res)
-              wx.navigateBack()
-            },
-            fail: err =>{
-              console.log(err)
-            }
-          })
-        },
-        fail: err =>{
-          console.log(err)
+    && time_ddl.getTime()<this.data.request_detail.time.getTime())
+    {
+      if (this.data.request_detail.type==1 || (this.data.request_detail.type==3 && this.data.request_detail.teams_to_vote.length==this.data.request_detail.voted_accept.length)){
+        if (this.data.request_detail.type==1){
+          var new_state = 2
+          var addition_words = ''
         }
-      })
+        else if (this.data.request_detail.type==3){
+          var new_state = 4
+          var addition_words = '组内'
+        }
+
+        wx.cloud.callFunction({
+          name: "update_request",
+          data:{
+            request:this.data.request_detail,
+            new_state: new_state,
+            to_delete: false
+          },
+          success: res => {
+            app.globalData.request_detail.state = 2
+  
+            var text_email = "原比赛: " + app.globalData.request_detail.month+'月'+app.globalData.request_detail.date+'日 ' 
+            + app.globalData.request_detail.hour+':'+app.globalData.request_detail.minute + " "
+            + app.globalData.request_detail.home_team + " VS "+ app.globalData.request_detail.away_team
+            + ' ' + app.globalData.request_detail.place + '\n' 
+            + "调整后比赛: "+ app.globalData.request_detail.month_new+'月'+app.globalData.request_detail.date_new+'日 ' 
+            + app.globalData.request_detail.hour_new+':'+app.globalData.request_detail.minute_new + " "
+            + app.globalData.request_detail.home_team + " VS "+ app.globalData.request_detail.away_team
+            + ' ' + app.globalData.request_detail.place_new + '\n'
+            + "申请日期: "+app.globalData.request_detail.month_req+'月'+app.globalData.request_detail.date_req+'日 ' 
+            + app.globalData.request_detail.hour_req+':'+app.globalData.request_detail.minute_req + '\n'
+            + "申请方: "+app.globalData.request_detail.requester +'\n'
+            + "申请类型: "+app.globalData.TYPES[app.globalData.request_detail.type-1] + '\n'
+            + "组别: " + app.globalData.request_detail.group + '\n'
+            console.log(text_email)
+            wx.cloud.callFunction({
+              name: 'send_email',
+              data:{
+                text: text_email,
+                attachment: 0,
+                subject: app.globalData.TYPES[app.globalData.request_detail.type-1]+'申请 '+ app.globalData.request_detail.home_team + " VS "+ app.globalData.request_detail.away_team + " (" + addition_words + "协商成功)"
+              },
+              success: res =>{
+                console.log(res)
+                wx.navigateBack()
+              },
+              fail: err =>{
+                console.log(err)
+              }
+            })
+          },
+          fail: err =>{
+            console.log(err)
+          }
+        })
+      }
+      else{
+        wx.navigateBack()
+      }
     }
     else{
       wx.cloud.callFunction({
@@ -130,6 +108,42 @@ Page({
     }
   },
 
+  accept(){
+    if(this.data.loading) return
+    this.setData({
+      loading: true
+    })
+
+    if (this.data.request_detail.is_vote){
+      var team_this = app.globalData.leader_info.team
+      var voted_accept = this.data.request_detail.voted_accept
+      voted_accept.push(team_this)
+      var voted_reject = this.data.request_detail.voted_reject
+
+      wx.cloud.callFunction({
+        name: "vote_request",
+        data:{
+          request: this.data.request_detail,
+          voted_accept: voted_accept,
+          voted_reject: voted_reject
+        },
+        success: res=>{
+          console.log(res)
+          this.data.request_detail.voted_accept = voted_accept
+          this.checkRequestValidation()
+        },
+
+        fail: err=>{
+          console.log(res)
+          return
+        }
+      })
+    }
+    else{
+      this.checkRequestValidation()
+    }
+  },
+
   recall(){
     if(this.data.loading) return
     this.setData({
@@ -144,9 +158,25 @@ Page({
         to_delete: true,
       },
       success: res => {
-        wx.navigateBack({
-          delta: 0,
-        })
+        wx.navigateBack()
+      },
+      fail: err =>{
+        console.log(err)
+      }
+    })
+  },
+
+  rejectRequest(){
+    app.globalData.request_detail.state = 0
+    wx.cloud.callFunction({
+      name: "update_request",
+      data:{
+        request:this.data.request_detail,
+        new_state: 0,
+        to_delete: false,
+      },
+      success: res => {
+        wx.navigateBack()
       },
       fail: err =>{
         console.log(err)
@@ -175,29 +205,15 @@ Page({
         },
         success: res=>{
           console.log(res)
-          wx.navigateBack()
+          this.rejectRequest()
         }
       })
     }
-
-    app.globalData.request_detail.state = 0
-    wx.cloud.callFunction({
-      name: "update_request",
-      data:{
-        request:this.data.request_detail,
-        new_state: 0,
-        to_delete: false,
-      },
-      success: res => {
-        wx.navigateBack({
-          delta: 0,
-        })
-      },
-      fail: err =>{
-        console.log(err)
-      }
-    })
+    else{
+      this.rejectRequest()
+    }
   },
+
   /**
    * 生命周期函数--监听页面加载
    */
