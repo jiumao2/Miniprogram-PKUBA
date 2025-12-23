dc6c2552d192fe5beebf51a9bbd61b7967c46598
PKU-scj <1033841550@qq.com>
2025-12-23 11:19:18 +0800

    小改动

--- cloudfunctions/get_knockout/index.js
+++ cloudfunctions/get_knockout/index.js
@@ -11,6 +11,7 @@ exports.main = async (event, context) => {
   })
   var tempname = []
   var tempscore = []
+  const keywards = ['决赛','半决赛','淘汰赛']
   var final = 0
   if (event.group == "男篮"){
     tempname = [["化学","元培","信科","生科","光经","工学","物理","医学"],Array(4),Array(2),Array(1)]
@@ -48,7 +49,7 @@ exports.main = async (event, context) => {
       let game = await db.collection('Schedule').where({
         home_team: tempname[_][i],
         away_team: tempname[_][i+1],
-        description: _==final-1?event.group+"决赛":"淘汰赛",
+        description: db.command.in(keywards),
         group: event.group
       }).get()
       if (game.data.length>0){
